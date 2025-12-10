2666c5b116ba887a2e0c05686c5ff4ab6f6a9d9a
polit8647 <polit8647@gmail.com>
2025-12-10 18:44:08 +0900

    Obstacle spin blade free axis mode update / obstacle prefab +2 / obstacle pool ready commit

---
+++ Assets/02.Scripts/Map/Obstacle/ObstacleSpawner.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    [Header("Obstacle Spawn Settings")]
+    [SerializeField] private List<GameObject> obstaclePrefabs;
+
+}
