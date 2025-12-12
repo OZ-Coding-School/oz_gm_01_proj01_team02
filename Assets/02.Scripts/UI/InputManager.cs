76e60bd3bd9f9d0b3bf5b820b3b526538006881c
김민식 <kysefms@gmail.com>
2025-12-12 18:12:58 +0900

    ui-option commit

--- Assets/02.Scripts/UI/InputManager.cs
+++
@@ -1,8 +0,0 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public static class InputManager
-//     {
-//         public static bool canScroll = true;
-//     }
