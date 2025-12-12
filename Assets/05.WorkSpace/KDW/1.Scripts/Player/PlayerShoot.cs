1804e661faa107e17ac6d6a0a5cf3aa76444fff3
김동욱 <kdso3175@naver.com>
2025-12-12 18:11:01 +0900

    feat: enemysearch-attack

--- Assets/05.WorkSpace/KDW/1.Scripts/Player/PlayerShoot.cs
+++ Assets/05.WorkSpace/KDW/1.Scripts/Player/PlayerShoot.cs
@@ -1,92 +1,37 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
-    [Header("프리팹")]
+    [Header("프리팹/생성위치")]
     [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform bulletPos;
 
-    [Header("적 감지")]
-    [SerializeField] private float checkRange;  //적과의 거리
-    [SerializeField] private LayerMask enemyLayer;
-    [SerializeField] private LayerMask layer;
+    private PlayerEnemySearch enemySearch;
+    private PlayerMove player;
 
-    private GameObject closeEnemy; //제일 가까운 적을 담아둘 변수
-    private bool isEnemy = false;
+    private Rigidbody rb;
 
-    private Vector3 enemyDir;
+    private Vector3 playerVec;
 
-    void Start()
+    private void Awake()
     {
-        
+        enemySearch = GetComponent<PlayerEnemySearch>();
+        player = GetComponent<PlayerMove>();
+
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        ShootBullet();
     }
 
-    void Update()
+    public void ShootBullet()
     {
-        EnemyCheck();
-    }
-
-    private void EnemyCheck()
-    {
-        Collider[] Colliders = Physics.OverlapSphere(transform.position, checkRange, enemyLayer);
-
-        if (Colliders.Length == 0)
+        if (enemySearch.CloseEnemy != null && rb.velocity.sqrMagnitude < 0.0001f)
         {
-            return;
+            GameObject bullet = Instantiate(bulletPrefab, bulletPos.position, Quaternion.identity);
+            bullet.transform.forward = player.EnemyDir;
         }
-
-        float minDis = Mathf.Infinity;  //제일 가까운 거리 저장
-        GameObject nearEnemy = null;
-        Vector3 currentPos = transform.position;
-
-        foreach (Collider collider in Colliders)
-        {
-            float dis = Vector3.Distance(collider.transform.position, currentPos);
-
-            if (dis <  minDis)
-            {
-                minDis = dis;
-                nearEnemy = collider.gameObject;
-            }
-        }
-        closeEnemy = nearEnemy;
-
-        EnemyLook(closeEnemy);
-
-        if (closeEnemy != null && isEnemy == true)
-        {
-            Shoot();
-        }
-    }
-    private bool EnemyLook(GameObject closeEnemy)
-    {
-        enemyDir = closeEnemy.transform.position - transform.position;
-        //isEnemy = Physics.Raycast(transform.position, enemyDir, enemyLayer);
-        RaycastHit hit;
-
-        if (Physics.Raycast(transform.position, enemyDir, out hit, enemyLayer))
-        {
-            isEnemy = true;
-            //Debug.Log(hit.collider.name);
-        }
-        else if (Physics.Raycast(transform.position, enemyDir, out hit, layer))
-        {
-            isEnemy = false;
-            //Debug.Log(hit.collider.name);
-        }
-        return isEnemy;
-    }
-    private void Shoot()
-    {
-        Debug.Log(closeEnemy.name);
-    }
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, checkRange);
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawRay(transform.position, enemyDir);
     }
 }
