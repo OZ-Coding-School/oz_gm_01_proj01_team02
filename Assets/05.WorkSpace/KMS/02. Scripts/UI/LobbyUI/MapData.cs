99d02dfa54918bd08e3a7e066a654d93dfb3f1ec
김민식 <kysefms@gmail.com>
2025-12-17 17:56:09 +0900

    HUD 구현

---
+++ Assets/05.WorkSpace/KMS/02. Scripts/UI/LobbyUI/MapData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapData : MonoBehaviour
+{
+    public string mapName;
+    public string sceneName;
+    public Sprite mapSprite;
+}
