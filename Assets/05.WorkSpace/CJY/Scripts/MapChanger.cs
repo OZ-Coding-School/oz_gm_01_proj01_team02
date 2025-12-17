e55ff418df73181280ae9e28efd7d38dce19b818
polit8647 <polit8647@gmail.com>
2025-12-17 17:20:15 +0900

    Desert, Hell Map Add Commit

---
+++ Assets/05.WorkSpace/CJY/Scripts/MapChanger.cs
@@ -0,0 +1,33 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
+
+////Ã©ÅÍº° ¾À º¯È¯ Å×½ºÆ®
+
+//public class MapChanger : MonoBehaviour
+//{
+//    public string[] stageName = { "Stage_01_Scene", "Stage_02_Scene", "Stage_03_Scene" };
+
+
+//    void Update()
+//    {
+//        if (Input.GetKeyDown(KeyCode.Alpha1))
+//        {
+//            SceneManager.LoadScene(stageName[0]);
+//        }
+//        if (Input.GetKeyDown(KeyCode.Alpha2))
+//        {
+//            SceneManager.LoadScene(stageName[1]);
+//        }
+//        if (Input.GetKeyDown(KeyCode.Alpha3))
+//        {
+//            SceneManager.LoadScene(stageName[2]);
+//        }
+//    }
+
+//    public void LoadScene()
+//    {
+
+//    }
+//}
