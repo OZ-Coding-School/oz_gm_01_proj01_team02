6109e6a2129a39a044e00c8a42c7d4c63cf9e491
김민식 <kysefms@gmail.com>
2025-12-23 18:00:53 +0900

    슬롯머신 인게임 적용

--- Assets/05.WorkSpace/KMS/02. Scripts/Test/TestEnemy.cs
+++ Assets/05.WorkSpace/KMS/02. Scripts/Test/TestEnemy.cs
@@ -1,16 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TestEnemy : MonoBehaviour
 {
-    void Start()
+    [Header("Enemy Stats")]
+    public float maxHp = 1000f;  // 테스트용으로 체력 높게 설정
+    public float currentHp;
+
+    private void Awake()
     {
-        
+        currentHp = maxHp;
     }
 
-    void Update()
+    // 데미지 받기
+    public void TakeDamage(float amount)
     {
-        
+        currentHp -= amount;
+        Debug.Log($"{gameObject.name} took {amount} damage. Current HP: {currentHp}");
+
+        if (currentHp <= 0)
+        {
+            Die();
+        }
+    }
+
+    // 죽음 처리
+    private void Die()
+    {
+        Debug.Log($"{gameObject.name} died.");
+        gameObject.SetActive(false); // 풀링용 비활성화
     }
 }
