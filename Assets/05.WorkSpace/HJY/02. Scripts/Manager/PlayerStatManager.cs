9b241e852fda4ab6a9f9c5cd816965d2e828a5f5
HamoHamoHamo <hamo222@naver.com>
2025-12-23 18:22:19 +0900

    fix: 중복 제거

--- Assets/05.WorkSpace/HJY/02. Scripts/Manager/PlayerStatManager.cs
+++ Assets/05.WorkSpace/HJY/02. Scripts/Manager/PlayerStatManager.cs
@@ -79,66 +79,51 @@ public class PlayerStatManager : MonoBehaviour
 
     }
 
-    public void ResetStat()
-    {
-        maxHp = baseMaxHp;
-        attack = baseAttack;
-        attackSpeed = baseAttackSpeed;
-        critRate = baseCritRate;
-        critDamage = baseCritDamage;
-        moveSpeed = baseMoveSpeed;
-        superTime = baseSuperTime;
-
-        dodgeRate = 0;
-        coinBonus = 0;
-
-    }
-
     private void ApplyStats(StatValue[] stats)
     {
         foreach (var stat in stats)
         {
             ApplyStat(stat);
         }
-            
+
     }
 
     public void ApplyStat(StatValue stat)
     {
         switch (stat.statType)
         {
-            case StatType.MaxHP: 
-                maxHp += stat.value; 
+            case StatType.MaxHP:
+                maxHp += stat.value;
                 break;
-            case StatType.MaxHpPercent: 
-                maxHp *= (1 + stat.value); 
+            case StatType.MaxHpPercent:
+                maxHp *= (1 + stat.value);
                 break;
-            case StatType.Attack: 
-                attack += stat.value; 
+            case StatType.Attack:
+                attack += stat.value;
                 break;
-            case StatType.AttackSpeed: 
-                attackSpeed += stat.value; 
+            case StatType.AttackSpeed:
+                attackSpeed += stat.value;
                 break;
-            case StatType.AttackSpeedPercent: 
-                attackSpeed *= (1 + stat.value); 
+            case StatType.AttackSpeedPercent:
+                attackSpeed *= (1 + stat.value);
                 break;
-            case StatType.CritRate: 
-                critRate += stat.value; 
+            case StatType.CritRate:
+                critRate += stat.value;
                 break;
-            case StatType.CritDamage: 
-                critDamage += stat.value; 
+            case StatType.CritDamage:
+                critDamage += stat.value;
                 break;
-            case StatType.MoveSpeed: 
-                moveSpeed += stat.value; 
+            case StatType.MoveSpeed:
+                moveSpeed += stat.value;
                 break;
-            case StatType.SuperTime: 
-                superTime += stat.value; 
+            case StatType.SuperTime:
+                superTime += stat.value;
                 break;
-            case StatType.DodgeRate: 
-                dodgeRate += stat.value; 
+            case StatType.DodgeRate:
+                dodgeRate += stat.value;
                 break;
-            case StatType.CoinBonus: 
-                coinBonus += stat.value; 
+            case StatType.CoinBonus:
+                coinBonus += stat.value;
                 break;
         }
     }
