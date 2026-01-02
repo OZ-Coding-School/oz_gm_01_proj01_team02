92c505c2bac617bfa9d26956a7634d6648746a40
polit8647 <polit8647@gmail.com>
2026-01-02 10:25:20 +0900

    feat : heal item func commit

--- Assets/05.WorkSpace/CJY/Scripts/Item/ItemS/Health.cs
+++ Assets/05.WorkSpace/CJY/Scripts/Item/ItemS/Health.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Health : ItemBase
 {
+    PlayerHealth playerHP;
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        playerHP = player.GetComponent<PlayerHealth>();
+    }
 
     public override void ReturnPool()
     {
+        playerHP.Heal(100);
         if (PoolManager.pool_instance != null) PoolManager.pool_instance.ReturnPool(this);
     }
 }
